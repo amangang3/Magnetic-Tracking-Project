9bf9b942d6593a6af6e02f6b5c72e81a6a4c96c2
amangang3 <aman.ganglani.16@ucl.ac.uk>
2020-01-24 19:42:48 +0000

    updated a few typos

--- Investigations/Investigation_5/experiment_1_setup_script.m
+++ Investigations/Investigation_5/experiment_1_setup_script.m
@@ -10,37 +10,35 @@ Power_supply_code(specan, 2, 10, 4, 1)
 %turn on channel 4 to power motor driver
 Power_supply_code(specan, 4, 12, 2, 1)
 %turn on channel 3 to drive the coil forwards
-Power_supply_code(specan, 3, 12, 2, 1)
+Power_supply_code(specan, 3, 12, 4, 1)
 
 %initialize magnetometer reading matrix
 magnetometer_data_averaged = zeros(25,3);
 
-%move the motor, take readings, assuming -ve takes motor away from coil, +ve takes motor towards coil (change later as needed)
+%move the motor, take readings, assuming -ve takes motor towards coil
 for reading = 1:25
-    disp(reading))
+    disp(reading);
     %if it's the 1st coil, just take the reading
-    if reading = 1
-        incoming_mag_data = collect_5_second_reading();
+    if reading == 1
+        incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
         magnetometer_data_averaged(reading,:) = mean(incoming_mag_data,1);
     end
 
     %for 1<reading<=13 move the thing 20mm away from coil and take reading
     if (reading>1) && (reading<=13)
-        program_motor(-20)
-        incoming_mag_data = collect_5_second_reading();
+        program_motor(20)
+        incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
         magnetometer_data_averaged(reading,:) = mean(incoming_mag_data,1);
     end
 
     %for reading > 13 move the thing 20mm toward the coil and take reading 
     if reading > 13
         program_motor(20)
-        incoming_mag_data = collect_5_second_reading();
+        incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
         magnetometer_data_averaged(reading,:) = mean(incoming_mag_data,1);
     end
 end
 
 disp('Experiment is complete')
-
-
 
 
