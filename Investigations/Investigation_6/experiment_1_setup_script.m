ee81fc9d70ca08530943e4e8056cac0a8513fe41
amangang3 <amangang3@gmail.com>
2020-01-28 20:02:25 +0000

    completed writing the code, it's messy but I think it will work

--- Investigations/Investigation_6/experiment_1_setup_script.m
+++ Investigations/Investigation_6/experiment_1_setup_script.m
@@ -13,32 +13,78 @@ Power_supply_code(specan, 4, 12, 2, 1)
 % Power_supply_code(specan, 3, 12, 4, 1)
 
 %turn on channel 1 to drive the coil backwards
-Power_supply_code(specan, 1, 12, 4, 1) 
+% Power_supply_code(specan, 1, 12, 4, 1) 
 
 %initialize magnetometer reading matrix
-magnetometer_data_averaged = zeros(21,3);
+forward_readings_averaged = zeros(21,3);
+backward_readings_averaged = zeros(21,3);
 
 %move the motor, take readings, assuming -ve takes motor towards coil
 for reading = 1:21
     disp(reading);
     %if it's the 1st coil, just take the reading
     if reading == 1
+        pause(5)
+        %turn forward coil on
+        Power_supply_code(specan, 3, 12, 4, 1)
+        %collect readings
         incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
-        magnetometer_data_averaged(reading,:) = mean(incoming_mag_data,1);
+        forward_readings_averaged(reading,:) = mean(incoming_mag_data,1);
+        %turn forward coil off
+        Power_supply_code(specan, 3, 12, 4, 0)
+        %turn backward coil on 
+        Power_supply_code(specan, 1, 12, 4, 1)
+        %wait 5 seconds
+        pause(5)
+        %collect reading
+        incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
+        backward_readings_averaged(reading,:) = mean(incoming_mag_data,1);
+        %turn backward coil off
+        Power_supply_code(specan, 1, 12, 4, 0)
     end
 
     %for 1<reading<=11 move the thing 20mm away from coil and take reading
     if (reading>1) && (reading<=11)
         program_motor(20)
+        pause(5)
+        %turn forward coil on
+        Power_supply_code(specan, 3, 12, 4, 1)
+        %collect readings
         incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
-        magnetometer_data_averaged(reading,:) = mean(incoming_mag_data,1);
+        forward_readings_averaged(reading,:) = mean(incoming_mag_data,1);
+        %turn forward coil off
+        Power_supply_code(specan, 3, 12, 4, 0)
+        %turn backward coil on 
+        Power_supply_code(specan, 1, 12, 4, 1)
+        %wait 5 seconds
+        pause(5)
+        %collect reading
+        incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
+        backward_readings_averaged(reading,:) = mean(incoming_mag_data,1);
+        %turn backward coil off
+        Power_supply_code(specan, 1, 12, 4, 0)
     end
 
     %for reading > 11 move the thing 20mm toward the coil and take reading 
     if reading > 11
         program_motor(-20)
+        pause(5)
+        %turn forward coil on
+        Power_supply_code(specan, 3, 12, 4, 1)
+        %collect readings
         incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
-        magnetometer_data_averaged(reading,:) = mean(incoming_mag_data,1);
+        forward_readings_averaged(reading,:) = mean(incoming_mag_data,1);
+        %turn forward coil off
+        Power_supply_code(specan, 3, 12, 4, 0)
+        %turn backward coil on 
+        Power_supply_code(specan, 1, 12, 4, 1)
+        %wait 5 seconds
+        pause(5)
+        %collect reading
+        incoming_mag_data = collect_5_second_reading(sparkfun_MPU);
+        backward_readings_averaged(reading,:) = mean(incoming_mag_data,1);
+        %turn backward coil off
+        Power_supply_code(specan, 1, 12, 4, 0)
     end
 end
 
